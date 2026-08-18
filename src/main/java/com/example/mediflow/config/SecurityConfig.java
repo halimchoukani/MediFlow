2bf0adf636663eab0a96a084e69200f21058304b
Halim Choukani <halim.choukani@gmail.com>
2026-08-18 22:51:07 +0100

    Adding login path to the security bean and testing the endpoint with insomnia ✅

--- src/main/java/com/example/mediflow/config/SecurityConfig.java
+++ src/main/java/com/example/mediflow/config/SecurityConfig.java
@@ -21,7 +21,7 @@ public class SecurityConfig {
         http
                 .csrf(csrf -> csrf.disable())
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/api/v1/auth/register").permitAll()
+                        .requestMatchers("/api/v1/auth/register","/api/v1/auth/login").permitAll()
                         .anyRequest().authenticated()
                 );
 
